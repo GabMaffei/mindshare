b3f55f5823d68dae8febcbaf6b5ab3f2056f72c0
Gabriel Maffei de Almeida <gabriel.maffei@portobello.com.br>
2026-04-26 19:44:24 -0300

    fix: change userService initialization to a private property

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -4,7 +4,7 @@ import { UserService } from "../services/user.service";
 
 @Resolver(() => UserModel)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  private userService = new UserService();
 
   @Query(() => UserModel)
   async getUser(@Arg("id", () => String) id: string): Promise<UserModel> {
