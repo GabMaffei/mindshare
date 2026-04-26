6d748bb855cf2fa0c61653056d2021f4da5cd76f
Gabriel Maffei de Almeida <gabriel.maffei@portobello.com.br>
2026-04-26 17:34:45 -0300

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import 'reflect-metadata'
+import express from "express"
+import { ApolloServer } from "@apollo/server"
+import { buildSchema } from "type-graphql"
+import { expressMiddleware } from "@as-integrations/express5"
+import { AuthResolver } from './resolvers/auth.resolver'
+import { UserResolver } from './resolvers/user.resolver'
+
+async function bootstrap() {
+    const app = express()
+
+    const schema = await buildSchema({
+        resolvers: [AuthResolver, UserResolver],
+        validate: false,
+        emitSchemaFile: './schema.graphql'
+    })
+
+    const server = new ApolloServer({
+        schema
+    })
+
+    await server.start()
+
+    app.use('/graphql', express.json(), expressMiddleware(server))
+
+    app.listen(4000, () => {
+        console.log('Server is running on http://localhost:4000/graphql')
+    })
+}
+
+bootstrap()
---
+++ src/resolvers/user.resolver.ts
@@ -0,0 +1,13 @@
+import { Arg, Query, Resolver } from "type-graphql";
+import { UserModel } from "../models/user.model";
+import { UserService } from "../services/user.service";
+
+@Resolver(() => UserModel)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Query(() => UserModel)
+  async getUser(@Arg("id", () => String) id: string): Promise<UserModel> {
+    return this.userService.findUser(id);
+  }
+}
